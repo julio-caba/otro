cd91a93ad2f437710a1e4454868eb08cf39a9a25
julio-caba <102553343+julio-caba@users.noreply.github.com>
2022-04-13 10:10:30 -0300

    Add files via upload

---
+++ testmail.php
@@ -0,0 +1,11 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting( E_ALL );
+$from = "test@mail.com";
+$to = "juliocaba@gmail.com";
+$subject = "chequeo de envio de email";
+$message = "se envio correctamente";
+$headers = "From: " . $from;
+mail($to, $subject, $message, $headers);
+echo "se envio el mensaje";
+?>
